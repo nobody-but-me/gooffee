9525fdd82a1171755dc2cfacef652396be042c5a
boring-nerd-dot-py <nunesteixeiradaniel12@gmail.com>
2025-02-08 01:23:18 -0300

    Better panning movement and stylized

--- panning_movement.js
+++ panning_movement.js
@@ -1,43 +1,39 @@
 (() => {
     
-    const _panning_container = document.getElementById("panning-container");
-    const _panning_content   = document.getElementById("house");
-    const _distance        = 150;
-    const _scrolling_speed = 10;
+    var _is_mouse_pressed = false;
+    var _initial_scroll_x = 0;
+    var _initial_scroll_y = 0;
+    var _initial_mouse_x  = 0;
+    var _initial_mouse_y  = 0;
     
-    var _mouse_x = 0;
-    var _mouse_y = 0;
-
-    var _translate_x = 0, _translate_y = 0;
-    var _dragging   = false;
-    var _start_x, _start_y;
-    
-    function _get_mouse_event(event) {
-	_mouse_x = event.clientX;
-	_mouse_y = event.clientY;
+    function _is_mouse_down(event) {
+	if (event.button === 0) {
+            _is_mouse_pressed = true;
+	    
+            _initial_scroll_x = window.scrollX;
+            _initial_scroll_y = window.scrollY;
+            _initial_mouse_x = event.clientX;
+            _initial_mouse_y = event.clientY;
+	    
+            document.body.style.cursor = "grabbing";
+	}
     }
+    function _is_mouse_up(event) {
+	if (event.button === 0) {
+            _is_mouse_pressed = false;
+            document.body.style.cursor = "default";
+	}
+    }
+    
     function _screen_movement(event) {
-	
-	const _window_width = window.innerWidth;
-	const _window_height = window.innerHeight;
-	
-	if (_mouse_x < _distance) {
-            window.scrollBy(-_scrolling_speed, 0);
-	}
-	else if (_mouse_x > _window_width - _distance) {
-            window.scrollBy(_scrolling_speed, 0);
-	}
-	
-	if (_mouse_y < _distance) {
-            window.scrollBy(0, -_scrolling_speed);
-	}
-	else if (_mouse_y > _window_height - _distance) {
-            window.scrollBy(0, _scrolling_speed);
+	if (_is_mouse_pressed) {
+            const _delta_x = _initial_mouse_x - event.clientX;
+            const _delta_y = _initial_mouse_y - event.clientY;
+	    
+            window.scrollTo(_initial_scroll_x + _delta_x, _initial_scroll_y + _delta_y);
 	}
     }
-    document.addEventListener('mousemove', _get_mouse_event);
-    setInterval(function() {
-	_screen_movement();
-    }, 10);
-    
+    document.addEventListener('mousemove', _screen_movement);
+    document.addEventListener('mousedown', _is_mouse_down);
+    document.addEventListener('mouseup',   _is_mouse_up);
 })();
