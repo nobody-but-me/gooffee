ee8fe6571fbc8a1c9f2a1638dc5cdd95a80b8d91
boring-nerd-dot-py <nunesteixeiradaniel12@gmail.com>
2025-02-19 16:36:25 -0300

    Linguistic room, writing room, new images

---
+++ tree-view.js
@@ -0,0 +1,13 @@
+(() => {
+
+    var _toggler = document.getElementsByClassName("tree-caret");
+    var i;
+
+    for (i = 0; i < _toggler.length; i++) {
+	_toggler[i].addEventListener("click", function() {
+	    this.parentElement.querySelector(".tree-nested").classList.toggle("tree-active");
+	    this.querySelector(".tree-image").classList.toggle("tree-image-active");
+	});
+    }
+
+})();
